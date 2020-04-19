0c136f69b91b97f5b1f77212ccdcfc55c17bca50
haeguri <haegyun821@gmail.com>
2020-04-19 15:38:12 +0900

    01: 리팩토링 하기 전

---
+++ 01-play-outsourcing/data/invoices.js
@@ -0,0 +1,21 @@
+const invoices = [
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  },
+];
+
+export default invoices;
---
+++ 01-play-outsourcing/data/plays.js
@@ -0,0 +1,7 @@
+const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
+
+export default plays;
