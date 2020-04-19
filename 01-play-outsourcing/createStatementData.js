a51dfdd3ebed24baa252fbc250ec4627a348a0ff
haeguri <haegyun821@gmail.com>
2020-04-19 21:34:47 +0900

    상세 구현은 서브 클래스에서 하도록 처리

--- 01-play-outsourcing/createStatementData.js
+++ 01-play-outsourcing/createStatementData.js
@@ -58,8 +58,6 @@ class PerformanceCalculator {
   get volumeCredits() {
     let result = 0;
     result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
     return result;
   }
 }
@@ -82,5 +80,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
