b7349a38d8ac23a96c00a8f95b5bb0cc8f080549
haeguri <haegyun821@gmail.com>
2020-06-01 00:23:50 +0900

    apply beforeEach

--- 04-test/Province.spec.js
+++ 04-test/Province.spec.js
@@ -2,13 +2,22 @@ import sampleProvinceData from "./sampleProvinceData";
 import Province from "./Province";
 
 describe("province", () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  })
+
   it("shortfall", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toEqual(230);
+  })
+
+  it("change production", () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
   })
 })
