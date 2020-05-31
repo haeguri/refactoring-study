2e8d776d416b3f3ec12aedcdf29774ba35b5068f
haeguri <haegyun821@gmail.com>
2020-06-01 01:25:31 +0900

    finish

--- 04-test/Province.spec.js
+++ 04-test/Province.spec.js
@@ -20,4 +20,57 @@ describe("province", () => {
     expect(asia.shortfall).toEqual(-6);
     expect(asia.profit).toEqual(292);
   })
-})
+
+  it("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toEqual(-25);
+    expect(asia.profit).toEqual(0);
+  });
+
+  it("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toEqual(-26);
+    expect(asia.profit).toEqual(-10);
+  });
+
+  it("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shortfall).toEqual(NaN);
+    expect(asia.profit).toEqual(NaN);
+  })
+});
+
+describe("no producers", () => {
+  let noProducers;
+
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", () => {
+    expect(noProducers.shortfall).toEqual(30);
+  })
+
+  it("profit", () => {
+    expect(noProducers.profit).toEqual(0);
+  });
+});
+
+describe("string for producers", () => {
+  it("", () => {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      price: 20
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toEqual(0);
+  }) 
+});
